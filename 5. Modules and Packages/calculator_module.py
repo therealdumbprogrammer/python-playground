def573536ee334ad1e4d81e7598912fd6730f304
vimau <therealdumbprogrammer@gmail.com>
2024-11-03 20:36:53 +0530

    Day 3 - Python Modules

---
+++ 5. Modules and Packages/calculator_module.py
@@ -0,0 +1,12 @@
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b
---
+++ 5. Modules and Packages/main.py
@@ -0,0 +1,9 @@
+import calculator_module
+from calculator_module import add, mul
+import calculator_module as calc
+
+print(calculator_module.division(9, 3))
+
+print(calculator_module.add(2, 4))
+
+print(calc.mul(2, 4))
